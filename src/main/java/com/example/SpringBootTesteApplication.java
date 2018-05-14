70dfc52bf89f56c7b614ca77d25f398a0d3c1dcd
Everton Schneider <everton_schneider1@hotmail.com>
2018-05-14 17:41:00 -0300

    Correção Pom

--- src/main/java/com/example/SpringBootTesteApplication.java
+++ src/main/java/com/example/SpringBootTesteApplication.java
@@ -2,10 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringBootTesteApplication {
-
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(SpringBootTesteApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTesteApplication.class, args);
 	}
